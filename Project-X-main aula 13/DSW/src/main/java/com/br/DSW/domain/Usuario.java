3116c619ac63e6799a05c66fb526b024af437d58
viniciuscsdias <117119376+viniciuscsdias@users.noreply.github.com>
2022-11-01 00:38:34 -0300

    Add files via upload

---
+++ Project-X-main aula 13/DSW/src/main/java/com/br/DSW/domain/Usuario.java
@@ -0,0 +1,11 @@
+package com.br.DSW.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Usuario {
+    private Long id;
+    private String nome;
+}
